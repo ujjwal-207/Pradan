44d2f1d8ff9cea6498d45d3c59cc2826af361686
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-12 20:04:56 +0545

    setting footer

--- src/components/fotter.jsx
+++ src/components/fotter.jsx
@@ -4,10 +4,32 @@ import "../styles/footer.css"
 function Footer(){
     return(
        
-     
+     <div className="footer">
         <footer>
-        copy
+        <div>
+            <h2>Our company</h2>
+            <ul>
+                <li>About Us</li>
+                <li>Careers</li>
+                <li>FAQs</li>
+            </ul>
+        </div>
+        <div>
+            <h2>Contact</h2>
+            <ul>
+                <li>Help& Support</li>
+                <li>Become Our Restaurant Partner</li>
+            </ul>
+        </div>
+        <div>
+            <h2>Legal</h2>
+            <ul><li>Terms&Condition</li>
+            <li>Privacy and Policy</li>
+            </ul>
+            
+        </div>
         </footer>
+        </div>
        
     )
 }
