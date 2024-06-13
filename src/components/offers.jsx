3bf5a84118afa03c37ad1c3521467fc37492d19a
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-13 22:36:13 +0545

    added new function

---
+++ src/components/offers.jsx
@@ -0,0 +1,16 @@
+import "../styles/offer.css"
+
+function Offer(){
+return(
+    <div>
+        <div className="boundary">
+        <img src="/images/border.png"></img>
+        </div>
+        <div className="offer-items">
+            <h1>Get Food Deliverd</h1>
+
+        </div>
+    </div>
+)
+}
+export default Offer;
