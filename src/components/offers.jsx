6ffed5b8c9cb606d77ae1005b6d59cd8735f2079
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-14 11:56:18 +0545

    added some css

--- src/components/offers.jsx
+++ src/components/offers.jsx
@@ -7,7 +7,26 @@ return(
         <img src="/images/border.png"></img>
         </div>
         <div className="offer-items">
+        <div className="get-food">
             <h1>Get Food Deliverd</h1>
+        </div>
+        <div className="features">
+        <div className="minimum-order">
+            <img src="/images/goods.png"></img>
+            <h4>No minimum order</h4>
+        </div>
+        <div className="free-delivery">
+            <img src="/images/bike.png"></img>
+            <h4>Free Delivery for orders more than Rs1000</h4>
+        </div>
+        <div className="online-pay">
+            <img src="/images/online pay.png"></img>
+            <h4>Pay Online</h4>
+        </div>
+        </div>
+            
+            
+            
 
         </div>
     </div>
